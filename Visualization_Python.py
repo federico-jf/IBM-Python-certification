404e76bec7561a04ac09746a939f3ec08c419126
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2024-01-25 09:13:32 -0600

    Update Visualization_Python.py

--- Visualization_Python.py
+++ Visualization_Python.py
@@ -30,3 +30,22 @@ df_can.isnull().sum()
 
 #summary statistics
 df_can.describe()
+
+#filter columns Country
+df_can.Country
+df_can.[['Country', 1980, 1981,1982,1983,1984]]
+
+#filter Japon for years 1980 to 1984
+df_can.loc['Japan', [1980, 1981,1982,1983,1984]]
+
+#create a list named 'year using map function for years from 1990 to 2013. Then extract the data serues from dataframe df_can for Haiti using year list
+year = list(map(str,range(1990,2014)))
+haiti = df_can.loc['Haiti', year]
+
+#filter data where AreaName is Africa and RegName is Shouthern Africa, Display it as dataframe
+df_can[(df_can['AreaName']=='Africa') & (df_can['RegName']=='Southern Africa')]
+
+#find the top 3 countries that contributes the most to immigration to Canada in the year 2010
+df_can.sort_values(by='2010', ascending=False, axis=0, inplace=True)
+top_3=df_can.head(3)
+top_3
