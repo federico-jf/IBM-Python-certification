237e04e0eb451f6818bb5f564ee5c1a8c40f46c7
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2024-01-26 11:36:31 -0600

    Update Visualization_Python.py

--- Visualization_Python.py
+++ Visualization_Python.py
@@ -153,3 +153,80 @@ plt.show()
 #################################################################################
 #################################################################################
 
+
+import numpy as np
+import pandas as pd
+
+
+#import matplotlib
+#we are using the inline backend
+%matplotlib line
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.style.use('ggplot') # for ggplot-like style
+
+#Open the file
+df_can = pd.read_excel(
+  'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Canada.xlsx',
+  sheet_name='Canada by Citizenship',
+  skiprows=range(20),
+  skipfooter=2)
+
+df_can.head
+
+print(df_can.shape)
+
+#set Country as the index to plot charts easily, by referring to the country names as index values
+df_can.set_index('Country', inplace=True)
+
+#create list of years from 1080-2013
+years= list(map(str, range(1980,2014)))
+
+######## AREA PLOT
+########
+########
+
+#THIS IS THE CODE WHEN WE USE THE SCRIPTING LAYER (procedureal method, we use matplotlib)
+
+df_can.sort_values(by='Total', ascending=False, axis=0, inplace=True)
+top_5=df_can.head(5)
+top_5 = df_top5[years].transpose()
+
+df_top5.head()
+
+#now plot
+df_top5.index= df_top5.index.map(int) #change the index values to integer
+df_top5.plot(kind='area',
+             alpha=0.25,  #alpha changes the transparency
+             stacked=False,
+            figsize=(20,10))
+
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+# THIS IS CODE WHEN WE USE THE ARTIST LAYER (using axes): use the artist layer (use ax.) to create an unstacked plot of the 5 countries that contributed the least to immigration to Canada. Transparency value of 0.55
+
+df_least5 = df_can.tail(5)
+df_least5 = df_least5[years].transpose()
+df_least5.head()
+
+df_least5.index= df_least5.index.map(int) #change the index values to integer
+ax = df_least5.plot(kind='area',
+             alpha=0.55,  #alpha changes the transparency
+             stacked=False,
+            figsize=(20,10))
+
+ax.set_title('Immigration Trend of 5 Countries with Less Contribution to Immigration')
+ax.set_ylabel('Number of immigrants')
+ax.set_xlabel('Years')
+
+
+######## HISTOGRAM
+########
+########
+
+
