8c362fdf5e71565711c7fb83825834720a17d7ed
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2024-02-08 08:12:34 -0600

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -42,11 +42,30 @@ test = cdf[~msk]
 from sklearn import linear_model
 regr= linear_model. LinearRegression()
 train_x = np.asanyarray(train[['ENGINESIZE']])
-train_Y = np.asanyarray(train[['CO2EMISSIONS']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
 regr.fit(train_x,train_y)
 
 print('Coefficients: ', regr.coef_)
 print('Intercept: ', regr.intercept_)
+
+#plot the fit line over the data
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS, color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], 'r')
+plt.xlabel("Engine Size")
+plt.ylabel("Emission")
+plt.show()
+
+#evaluation of the model: compare actual values with the predicted values. (R2)
+from sklearn.metrics import r2_score
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+print('Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print('Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) **2))
+print('R2-score: %.2f" % r2_score(test_y - test_y_))
+
 
 
 
